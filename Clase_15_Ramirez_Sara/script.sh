c9c1c8ca3c7eb624ce1672512b5808fa83163078
ramirezandradesara <sararamirezandrade@gmail.com>
2021-08-03 12:43:58 -0300

    Agrego desde clase 1 a 15

---
+++ Clase_15_Ramirez_Sara/script.sh
@@ -0,0 +1,6 @@
+1) Algunos sistemas operativos no poseen compiladores para determinador lenguajes de programacion.
+2) Virtualbox es una virtual machine de sistemas que sirve de separacion del sistema operativo y las aplicaciones de la computadora fisica.
+3) No, porque cada una se comporta como una maquina separada, individual. Cada una se virtualiza de manera aislada.
+
+
+
--- Clase_5_Ramirez_Sara/RepositorioEjercitacionClase5/README.md
+++ Clase_5_Ramirez_Sara/RepositorioEjercitacionClase5/README.md
@@ -1,1 +1,2 @@
 Link repositorio sobre ejercitación de conflictos: https://github.com/ramirezandradesara/Repositorio_ejercitacion
+git 
